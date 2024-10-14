8bec8834fe5dae8f360214afebd5ccc3cc7860c9
Quang Akiyama <quanganhcf2@gmail.com>
2024-10-14 20:17:41 +0700

    update github linkedin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -58,10 +58,10 @@ const Header = () => {
 
           {/* Social links (hidden in mobile) */}
           <div className="hidden sm:flex flex-row gap-3">
-            <a href="">
+            <a href="https://github.com/tuanson007" target="_blank">
               <img src={GithubLogo} alt="GitHub" />
             </a>
-            <a href="">
+            <a href="https://www.google.com/" target="_blank">
               <img src={LinkedinLogo} alt="LinkedIn" />
             </a>
           </div>
@@ -90,10 +90,7 @@ const Header = () => {
                   <a href="https://github.com/tuanson007" target="_blank">
                     <img src={GithubLogo} alt="GitHub" />
                   </a>
-                  <a
-                    href="https://www.linkedin.com/in/đào-tuấn-sơn-713b4632b/"
-                    target="_blank"
-                  >
+                  <a href="https://www.google.com/" target="_blank">
                     <img src={LinkedinLogo} alt="LinkedIn" />
                   </a>
                 </div>
